f912a2ff481de5ab1d876d32c1f8e4b5c3a5d295
lualcs <1508786776@qq.com>
2021-07-09 06:49:14 -0700

    code

--- application/test/controller/Index.php
+++ application/test/controller/Index.php
@@ -3,10 +3,13 @@ namespace app\test\controller;
 
 use think\Controller;
 
+use app\test\module\Index as OModule;
+
 class Index extends Controller
 {
     public function index()
     {
-        return $this->view->fetch();
+        return json(OModule::select());//$this->view->fetch();;
     }
 }
+
---
+++ application/test/module/Index.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace app\test\module;
+
+use think\Model;
+
+class Index extends Model
+{
+    protected $table = 'dbusers.users';
+
+    protected static function init()
+    {
+    }
+}
